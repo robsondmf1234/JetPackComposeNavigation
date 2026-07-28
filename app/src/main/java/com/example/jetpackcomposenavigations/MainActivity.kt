fc0247a9269a8a2109184befaa47f7c8e4cb8bda
robsondmf <robsondmf1234@gmail.com>
2026-07-28 16:05:36 -0300

    feat - Refactor.

--- app/src/main/java/com/example/jetpackcomposenavigations/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposenavigations/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.jetpackcomposenavigations
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
