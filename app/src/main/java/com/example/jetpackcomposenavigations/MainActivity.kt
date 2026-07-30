1bf298e442aee7c213ab07913166c7ac469b0ad6
robsondmf <robsondmf1234@gmail.com>
2026-07-30 11:31:44 -0300

    feat - Refactor.

--- app/src/main/java/com/example/jetpackcomposenavigations/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposenavigations/MainActivity.kt
@@ -79,7 +79,7 @@ class MainActivity : ComponentActivity() {
                         bottomAppBarItemSelected = selectedItem,
                         onBottomAppBarItemSelectedChange = {
                             val route = it.route
-                            navController.navigate(route){
+                            navController.navigate(route) {
                                 launchSingleTop = true
                                 popUpTo(route)
                             }
