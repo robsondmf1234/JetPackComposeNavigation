db7f6a6ce921b06060c0b38af238a661ecab73f8
robsondmf <robsondmf1234@gmail.com>
2026-07-30 10:57:15 -0300

    feat - Ajustes no gerenciamento de telas no back stack.

--- app/src/main/java/com/example/jetpackcomposenavigations/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposenavigations/MainActivity.kt
@@ -79,7 +79,10 @@ class MainActivity : ComponentActivity() {
                         bottomAppBarItemSelected = selectedItem,
                         onBottomAppBarItemSelectedChange = {
                             val route = it.route
-                            navController.navigate(route)
+                            navController.navigate(route){
+                                launchSingleTop = true
+                                popUpTo(route)
+                            }
                         },
                         onFabClick = {
                         }) {
